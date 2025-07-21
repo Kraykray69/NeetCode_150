f8798cc6fdec07f6f6c3ad006b1b8c8765674a27
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-07-21 23:53:57 +0530

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         for i in range(len(nums)):
@@ -11,3 +12,20 @@ class Solution:
 list1 = Solution()       
 list1.hasDuplicate([1,2,3,4])
 
+
+
+# Solution 2
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        if len(nums) == 1:
+            return False
+        for i in range(len(nums)):
+            if nums[i-1] == nums[i]:
+                return True
+        return False
+
+
+
+list1 = Solution()       
+list1.hasDuplicate([1,2,3,4])
