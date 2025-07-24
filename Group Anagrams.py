d5c848ff4718c71c5b322cf944bebb9a9ae90fac
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-07-24 22:50:47 +0530

    Update Group Anagrams.py

--- Group Anagrams.py
+++ Group Anagrams.py
@@ -11,3 +11,21 @@ class Solution:
 
 grouping = Solution()
 grouping.groupAnagrams(['act','cat','pots','hat','stop','x','dinosaur'])
+
+
+
+# Solution 2
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            group[tuple(count)].append(s)
+
+        return list(group.values())
+
+
+grouping = Solution()
+grouping.groupAnagrams(['act','cat','pots','hat','stop','x','dinosaur'])
