cd3958f957075f72a791a06ff31fcb11b7af7996
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-07-26 23:23:56 +0530

    Update Top K Frequent Elements.py

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -19,3 +19,27 @@ class Solution:
 
 topKFreq = Solution()
 topKFreq.topKFrequent([1,2,2,4,4,3,3,3],2)
+
+
+
+# Solution 2
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq)-1, 0, -1):
+            for num in freq[i]:
+                if len(res) < k:
+                    res.append(num)
+        return res                        
+
+
+topKFreq = Solution()
+topKFreq.topKFrequent([1,2,2,4,4,3,3,3],2)  
