75ac37a1b3c3a6716cc626f6df4e01635125094a
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-07-29 21:38:43 +0530

    Update Product Of Array Except Self.py

--- Product Of Array Except Self.py
+++ Product Of Array Except Self.py
@@ -14,3 +14,32 @@ class Solution:
 
 prodself = Solution()
 prodself.productExceptSelf([-1,0,1,2,3])
+
+
+
+# Solution 2
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prod, zero_cnt = 1, 0
+        for num in nums:
+            if num:
+                prod *= num
+            else:
+                zero_cnt += 1
+        if zero_cnt > 1:
+            return [0] * len(nums)
+
+        res = [0] * len(nums)
+        for i in range(len(nums)):
+            if zero_cnt:
+                if nums[i]:
+                    res[i] = 0
+                else:
+                    res[i] = prod
+            else:
+                res[i] = prod // nums[i]
+        return res
+
+
+prodself = Solution()
+prodself.productExceptSelf([-1,0,1,2,3])
