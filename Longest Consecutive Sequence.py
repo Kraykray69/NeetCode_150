9bb86c7b3fddd8b655868a555bfc20cb45b1c040
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-07-31 22:18:57 +0530

    Update Longest Consecutive Sequence.py

--- Longest Consecutive Sequence.py
+++ Longest Consecutive Sequence.py
@@ -1,1 +1,23 @@
+# Solution 1
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) < 1:
+            return 0
 
+        check = set(nums)
+        longest = 0
+
+        for i in range(len(nums)):
+            current = nums[i]
+            count = 1
+            if current - 1 not in check:
+                while current + 1 in check:
+                    current += 1
+                    count += 1
+            longest = max(longest,count)
+
+        return longest
+
+
+longcons = Solution()
+longcons.longestConsecutive([2,20,4,10,3,4,5])
