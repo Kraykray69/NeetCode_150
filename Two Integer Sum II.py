1edf7ecffe00dab41a383cf3b7e957639a503642
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-08-04 19:30:03 +0530

    Create Two Integer Sum II.py

---
+++ Two Integer Sum II.py
@@ -0,0 +1,11 @@
+# Solution 1
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            for j in range(i+1,len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    return [i+1,j+1]
+
+
+twosum = Solution()
+twosum.twoSum([1,2,3,4],3)
