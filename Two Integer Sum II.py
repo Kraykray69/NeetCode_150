f5e9c79d4dec251be9476c428577016a26391b61
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-08-04 19:35:44 +0530

    Update Two Integer Sum II.py

--- Two Integer Sum II.py
+++ Two Integer Sum II.py
@@ -9,3 +9,20 @@ class Solution:
 
 twosum = Solution()
 twosum.twoSum([1,2,3,4],3)
+
+
+
+# Solution 2
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        check = {}
+        for i, c in enumerate(numbers):
+            check[c] = i
+        for i in range(len(numbers)):
+            difference = target - numbers[i]
+            if difference in check and i!=check[difference]:
+                return [i+1,check[difference]+1]
+            
+
+twosum = Solution()
+twosum.twoSum([1,2,3,4], 3)  
