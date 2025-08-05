e914fc6382ce60f899f7a95d7153195144b54ff2
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-08-05 19:09:14 +0530

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -17,3 +17,27 @@ threesum.threeSum([-1,0,1,2,-1,-4])
 
 
 
+
+# Solution 2
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        nums.sort()
+        for i in range(len(nums)-2):
+            l, r = i+1, len(nums)-1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                target = 0
+                if total < target:
+                    l += 1
+                elif total > target:
+                    r -= 1
+                else:
+                    res.add(tuple([nums[i],nums[l],nums[r]]))
+                    l, r = l+1, r-1
+
+        return list(res)
+
+
+threesum = Solution()
+threesum.threeSum([-1,0,1,2,-1,-4])
