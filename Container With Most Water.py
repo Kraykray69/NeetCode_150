c23179f18dc8d0217de12c86675d89fd98c4334e
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-08-06 20:57:04 +0530

    Update Container With Most Water.py

--- Container With Most Water.py
+++ Container With Most Water.py
@@ -11,3 +11,24 @@ class Solution:
 
 mostwater = Solution()
 mostwater.maxArea([1,7,2,5,4,7,3,6])
+
+
+
+
+# Solution 2
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        longest = 0
+        l, r = 0, len(heights) -1 
+        while l < r:
+            longest = max(longest,min(heights[l],heights[r]) * (r-l))
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return longest
+
+
+mostwater = Solution()
+mostwater.maxArea([1,7,2,5,4,7,3,6])
