ef03d9b31fdee059ce9efc21473949d275d93d37
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-08-08 00:19:17 +0530

    Update Trapping Rain Water.py

--- Trapping Rain Water.py
+++ Trapping Rain Water.py
@@ -21,3 +21,30 @@ class Solution:
 
 traprain = Solution()
 traprain.trap([0,2,0,3,1,0,1,3,2,1])
+
+
+
+
+# Solution 2
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        min_list = [0] * len(height)
+        min_list[0], min_list[len(height)-1] = height[0], height[len(height)-1]
+        res = 0
+        l, r = 0, len(height) - 1
+        while l < r:
+            if height[l] <= height[r]:
+                l += 1
+                min_list[l] = max(height[l],min_list[l-1])
+                res += min_list[l] - height[l]
+            elif height[l] > height[r]:
+                r -= 1
+                min_list[r] = max(height[r],min_list[r+1])
+                res += min_list[r] - height[r]
+
+        return res
+
+
+traprain = Solution()
+traprain.trap([0,2,0,3,1,0,1,3,2,1])
+        
