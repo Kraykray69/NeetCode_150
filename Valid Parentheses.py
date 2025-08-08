20999e65c0dc73cd666e365d8601507c39b17fc3
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-08-08 21:23:58 +0530

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -10,3 +10,25 @@ class Solution:
 
 val = Solution()
 val.isValid("([{}])")
+
+
+
+
+# Solution 2
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {'}':'{', ']':'[', ')':'('}
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+
+
+val = Solution()
+val.isValid("([{}])")
