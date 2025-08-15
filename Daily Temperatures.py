fbe29110bd802fbbfab3b920cd153f5af4dcbb86
Rupesh S <146557013+Kraykray69@users.noreply.github.com>
2025-08-16 02:11:15 +0530

    Update Daily Temperatures.py

--- Daily Temperatures.py
+++ Daily Temperatures.py
@@ -15,3 +15,24 @@ class Solution:
 
 dailytemp = Solution()
 dailytemp.dailyTemperatures([30,38,30,36,35,40,28])
+
+
+
+
+# Solution 2
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(temperatures)
+
+        for i, c in enumerate(temperatures):
+            while stack and c > stack[-1][1]:
+                index, temp = stack.pop()
+                res[index] = i - index
+            stack.append((i,c))
+
+        return res
+
+
+dailytemp = Solution()
+dailytemp.dailyTemperatures([30,38,30,36,35,40,28])
